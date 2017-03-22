6f2cae5dac9ae3435688dd4fc546b0e6eed6371d
Luke Kedz <lukekedziora@gmail.com>
2017-03-22 15:57:26 -0400

    validations in place. ready for live test over 24 hrs

--- scrape.rb
+++ scrape.rb
@@ -12,39 +12,49 @@ log.level = Logger::INFO
 output    = PrettifyLogOutput.new
 valid     = Validation.new
 
+validations = {}
+
 begin
-	# TODO: chron (every morning, mult times, from 7am - 11am or similar)
+	# TODO: cron (every morning, mult times, from 7am - 11am or similar)
     loop do
     	log.info output.start
     	log.info output.new_line
 
-        # page      = HTTParty.get('http://www.espn.com/nba/player/gamelog/_/id/3059318/joel-embiid')
-        page      = HTTParty.get('http://www.espn.com/nba/player/gamelog/_/id/3032978/dario-saric')
-        columns   = Nokogiri::HTML(page).css('.tablehead tr:nth-child(2) td')
-        last_game = Nokogiri::HTML(page).css('.tablehead tr:nth-child(3) td')
+        # espn_page             = HTTParty.get('http://www.espn.com/nba/player/gamelog/_/id/3059318/joel-embiid')
+        espn_page               = HTTParty.get('http://www.espn.com/nba/player/gamelog/_/id/3032978/dario-saric')
+        validations[:espn_page] = valid.page(log, espn_page.code)
 
-        # TODO: validations
-        # is the page http 200
-        # are the columns populated and matched?
-        # is there data present for the last game
-        # does the score category contain a W/L, indicating the game stat log is complete?
-        # heroku app => have we already scraped and uploaded this data?
-        # log.info "DATA VALIDATION: " valid_data.inspect
-    	# log.info output.new_line
+        columns               = Nokogiri::HTML(espn_page).css('.tablehead tr:nth-child(2) td')
+        validations[:columns] = valid.columns(log, columns)
 
-        stats = {}
+        last_game_in_espn_log       = Nokogiri::HTML(espn_page).css('.tablehead tr:nth-child(3) td')
+        validations[:game_complete] = valid.game_complete(log, last_game_in_espn_log)
 
-        columns.each_with_index do |value, index|
-            stats[value.text] = last_game[index].children.text
-        end
+		# TODO: could store this value within cron job
+        # last_stat_record                 = HTTParty.get('http://localhost:2121/site/last_stat_record')
+        last_stat_record                 = HTTParty.post('http://embiid21.herokuapp.com/site/last_game')
+        validations[:stats_not_uploaded] = valid.stats_not_uploaded(log, last_game_in_espn_log, last_stat_record)
 
-		valid_data = true
-        	
-		log.info "SCRAPED STATS INCOMING/RAW"
-        stats.keys.each_with_index do |key, index|
-        	log.info key.inspect + " => " + stats[key].inspect
+    	upload_data = []
+    	validations.each { |k,v| upload_data.push v }
 
-        	if key == valid.columns(index)
+		# TODO: condense into one line iteration combined w/ below conditional 'all?'
+    	log.info output.new_line
+    	log.info "VALIDATIONS: " + validations.inspect
+    	log.info "UPLOAD? " + upload_data.inspect
+    	log.info output.new_line
+
+    	if upload_data.all?
+	        stats = {}
+
+	        columns.each_with_index do |value, index|
+	            stats[value.text] = last_game_in_espn_log[index].children.text
+	        end
+	        	
+			log.info "SCRAPED STATS INCOMING/RAW"
+	        stats.keys.each_with_index do |key, index|
+	        	log.info key.inspect + " => " + stats[key].inspect
+
 	        	case key
 		            when 'DATE'
 		                stats['GAME_DATE'] = stats[key]
@@ -67,20 +77,12 @@ begin
 		            when 'FT%'
 		                stats['FT_PRCT'] = stats[key]
 		                stats.delete(key)
-		            end
-		    else
-		    	valid_data = false
-		    	break
-		    end
-        end
-    	log.info output.new_line
-
-        if valid_data
-	        log.info "SCRAPED STATS READY FOR UPLOAD"
-
-	        stats.each do |st|
-	        	log.info st.inspect
+		        end
 	        end
+
+	    	log.info output.new_line
+	        log.info "SCRAPED STATS READY FOR UPLOAD"
+	        stats.each { |st| log.info st.inspect }
 	    	log.info output.new_line
 
 	        # response = HTTParty.post('http://localhost:2121/site/upload_stats', {
@@ -91,10 +93,7 @@ begin
 
 	        log.info response.inspect
     		log.info output.new_line
-	    else
-	    	log.error "DATA INVALID: %p" % stats.inspect
-    		log.error output.new_line
-	    end
+    	end
 
 	   	# TODO: email update
         sleep(60 * 30)
@@ -106,7 +105,7 @@ rescue StandardError => error
 	log.error error.inspect
     log.error output.new_line
 
-	sleep(60 * 5)
+	sleep(60 * 30)
 	retry
 end
 
--- validation.rb
+++ validation.rb
@@ -1,9 +1,50 @@
 class Validation
 
-	def columns(index)
-		valid_columns = ['DATE', 'OPP', 'SCORE', 'MIN', 'FGM-FGA', 'FG%', '3PM-3PA', '3P%', 'FTM-FTA', 'FT%', 'REB', 'AST', 'BLK', 'STL', 'PF', 'TO', 'PTS']
+	def page(log, code)
+		log.info __method__
+		log.info code.inspect
 
-		valid_columns[index]
+		code == 200 ? true : false
+	end
+
+	def columns(log, columns)
+		log.info __method__
+		log.info columns.inspect
+
+		valid_columns = ['DATE',    'OPP', 'SCORE',   'MIN', 
+			             'FGM-FGA', 'FG%', '3PM-3PA', '3P%', 
+			             'FTM-FTA', 'FT%', 'REB',     'AST', 
+			             'BLK',     'STL', 'PF',      'TO',  
+			             'PTS'
+			          	]
+
+        columns.each_with_index do |value, index|
+			if value.text != valid_columns[index]
+				return false
+			end
+		end
+
+		true
+	end
+
+	def game_complete(log, last_game_in_espn_log)
+		log.info __method__
+		log.info last_game_in_espn_log.inspect
+
+		outcome = last_game_in_espn_log[2].children.text[0]
+
+		outcome == "W" || outcome == "L" ? true : false
+	end
+	
+	def stats_not_uploaded(log, last_game_in_espn_log, last_stat_record)
+		log.info __method__
+		log.info last_game_in_espn_log.inspect
+		log.info last_stat_record.inspect
+
+		last_game_date   = last_game_in_espn_log[0].children.text
+		last_record_date = last_stat_record.parsed_response["game_date"]
+
+		last_game_date == last_record_date ? false : true
 	end
 	
 end
