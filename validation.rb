33150eccc7869594c31c60c7e50c1a7104ba1ae1
Luke Kedz <lukekedziora@gmail.com>
2017-03-23 11:13:02 -0400

    adding init to Validation with arguments

--- validation.rb
+++ validation.rb
@@ -1,15 +1,22 @@
 class Validation
 
-	def page?(log, code)
-		log.info __method__
-		log.info code.inspect
+	def initialize(log, output)
+		@log    = log
+		@output = output
+	end
+
+	def page?(code)
+		@log.info __method__
+		@log.info code.inspect
+		@log.info @output.new_line
 
 		code == 200 ? true : false
 	end
 
-	def columns?(log, columns)
-		log.info __method__
-		log.info columns.inspect
+	def columns?(columns)
+		@log.info __method__
+		@log.info columns.inspect
+		@log.info @output.new_line
 
 		valid_columns = ['DATE',    'OPP', 'SCORE',   'MIN', 
 			             'FGM-FGA', 'FG%', '3PM-3PA', '3P%', 
@@ -27,19 +34,21 @@ class Validation
 		true
 	end
 
-	def game_complete?(log, last_game_in_espn_log)
-		log.info __method__
-		log.info last_game_in_espn_log.inspect
+	def game_complete?(last_game_in_espn_log)
+		@log.info __method__
+		@log.info last_game_in_espn_log.inspect
+		@log.info @output.new_line
 
 		outcome = last_game_in_espn_log[2].children.text[0]
 
 		outcome == "W" || outcome == "L" ? true : false
 	end
 	
-	def upload_stats?(log, last_game_in_espn_log, last_stat_record)
-		log.info __method__
-		log.info last_game_in_espn_log.inspect
-		log.info last_stat_record.inspect
+	def upload_stats?(last_game_in_espn_log, last_stat_record)
+		@log.info __method__
+		@log.info last_game_in_espn_log.inspect
+		@log.info last_stat_record.inspect
+		@log.info @output.new_line
 
 		last_game_date   = last_game_in_espn_log[0].children.text
 		last_record_date = last_stat_record.parsed_response["game_date"]
