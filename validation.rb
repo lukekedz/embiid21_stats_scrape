ec31583c2c925578a4eae21a76881d74bbe25806
Luke Kedz <lukekedziora@gmail.com>
2017-03-23 11:01:26 -0400

    added question mark to all validation methods to indicate boolean return. upd final validation method for better natural langauge interpretation, inc comments

--- validation.rb
+++ validation.rb
@@ -1,13 +1,13 @@
 class Validation
 
-	def page(log, code)
+	def page?(log, code)
 		log.info __method__
 		log.info code.inspect
 
 		code == 200 ? true : false
 	end
 
-	def columns(log, columns)
+	def columns?(log, columns)
 		log.info __method__
 		log.info columns.inspect
 
@@ -27,7 +27,7 @@ class Validation
 		true
 	end
 
-	def game_complete(log, last_game_in_espn_log)
+	def game_complete?(log, last_game_in_espn_log)
 		log.info __method__
 		log.info last_game_in_espn_log.inspect
 
@@ -36,7 +36,7 @@ class Validation
 		outcome == "W" || outcome == "L" ? true : false
 	end
 	
-	def stats_not_uploaded(log, last_game_in_espn_log, last_stat_record)
+	def upload_stats?(log, last_game_in_espn_log, last_stat_record)
 		log.info __method__
 		log.info last_game_in_espn_log.inspect
 		log.info last_stat_record.inspect
@@ -44,6 +44,7 @@ class Validation
 		last_game_date   = last_game_in_espn_log[0].children.text
 		last_record_date = last_stat_record.parsed_response["game_date"]
 
+		# if no match, then TRUE/proceed to upload new record
 		last_game_date == last_record_date ? false : true
 	end
 	
